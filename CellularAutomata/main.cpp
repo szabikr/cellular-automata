377ec3a58a09f3e1efd5c445bee9807d34d3aee5
Keresztes Szabolcs <keresztes93@yahoo.com>
2014-10-25 23:25:14 +0300

    Drawing the cellular automata on an image.

--- CellularAutomata/main.cpp
+++ CellularAutomata/main.cpp
@@ -5,12 +5,16 @@
 #include "CellularAutomata1D.h"
 #include "Rule.h"
 
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+using namespace cv;
 using namespace std;
 
 int main(void) {
 
 	CellularAutomata1D ca;
-	Rule rule; 
+	Rule rule;
 
 	ifstream f_ca("initial_ca.txt");
 
@@ -29,18 +33,38 @@ int main(void) {
 
 	ca.setRule(rule);
 
-	cout << ca << endl;
-	for (int i = 0; i < 70; ++i) {
-		ca.iterate(1);
-		cout << ca << endl;
-	}
-	ca.iterate(1);
+	
 
-	cout << "Did I change the master branch?" << endl; 
-	cout << "Did I made a new branch?" << endl;
-	cout << "Did I merged the two branches? - not yet" << endl;
-	cout << "This is added to the master branch." << endl;
-	cout << "Lets merge the branches" << endl;
+	Mat image(400, 300, CV_8UC3);
+
+	for (int i = 0; i < 400; ++i) {
+		ca.iterate(1);
+		vector<int> caStatus = ca.getCAStatus();
+		for (int j = 0; j < caStatus.size(); ++j) {
+			Vec3b color;
+			if (caStatus[j]) {
+				color[0] = 0;
+				color[1] = 0;
+				color[2] = 0;
+			}
+			else {
+				color[0] = 255;
+				color[1] = 255;
+				color[2] = 255;
+			}
+			image.at<Vec3b>(Point(j, i)) = color;
+		}
+	}
+
+	if (!image.data) {
+		cout << "Could not open or find the image" << endl;
+		return -1;
+	}
+
+	namedWindow("Display window", WINDOW_AUTOSIZE);
+	imshow("Display window", image);
+
+	waitKey(0);
 
 	return 0;
 }
